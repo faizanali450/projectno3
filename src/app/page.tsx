bd48be3241bac208bf802f4baa60b89b43438b37
faizanali450 <sikusalaar@gmail.com>
2025-08-28 13:03:34 +0500

    projectno3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
 const page = () => {
-  return (
+return (
+  <div>
     <div>
-      <figure className="md:flex-row bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      
+    </div>
+     <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/feroz.jpg" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “My name is Feroz A.I developer. I am a passionate developer with expertise in artificial intelligence and machine learning. I love creating innovative solutions that leverage the power of AI to solve real-world problems. With a strong background in programming and data analysis, I am dedicated to pushing the boundaries of technology and making a positive impact through my work.”
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
       </p>
     </blockquote>
     <figcaption className="font-medium">
@@ -21,12 +26,14 @@ const page = () => {
     </figcaption>
   </div>
 </figure>
-<figure className="md:flex-row bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+ <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/feroz.jpg" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “My name is Feroz A.I developer. I am a passionate developer with expertise in artificial intelligence and machine learning. I love creating innovative solutions that leverage the power of AI to solve real-world problems. With a strong background in programming and data analysis, I am dedicated to pushing the boundaries of technology and making a positive impact through my work.”
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
       </p>
     </blockquote>
     <figcaption className="font-medium">
@@ -39,12 +46,14 @@ const page = () => {
     </figcaption>
   </div>
 </figure>
-<figure className="md:flex-row bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/feroz.jpg" alt="" width="384" height="512"/>
+ <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/syco.jpg" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “My name is Feroz A.I developer. I am a passionate developer with expertise in artificial intelligence and machine learning. I love creating innovative solutions that leverage the power of AI to solve real-world problems. With a strong background in programming and data analysis, I am dedicated to pushing the boundaries of technology and making a positive impact through my work.”
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
       </p>
     </blockquote>
     <figcaption className="font-medium">
@@ -57,8 +66,68 @@ const page = () => {
     </figcaption>
   </div>
 </figure>
-</div>
-  )
+ <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/syco.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        Feroz A.I developer
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Feroz A.I developer
+      </div>
+    </figcaption>
+  </div>
+</figure>
+ <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/syco.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        Feroz A.I developer
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Feroz A.I developer
+      </div>
+    </figcaption>
+  </div>
+</figure>
+ <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/syco.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        Feroz A.I developer
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Feroz A.I developer
+      </div>
+    </figcaption>
+  </div>
+</figure>
+  </div>
+)
 }
 
 export default page
